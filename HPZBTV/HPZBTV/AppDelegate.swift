588562ce1bdf94dda18199b32cfd2f053e7a6ca9
AresPeng <jue2yin81991@163.com>
2017-03-21 17:10:51 +0800

    目录划分

--- HPZBTV/HPZBTV/AppDelegate.swift
+++ HPZBTV/HPZBTV/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    
+    //wode nanme
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
