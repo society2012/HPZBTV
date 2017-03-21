b44c8af7884df07b0dcb5c0395e5bda343ffaa3b
AresPeng <jue2yin81991@163.com>
2017-03-21 16:20:13 +0800

    新建项目

---
+++ HPZBTV/HPZBTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HPZBTV
+//
+//  Created by hupeng on 17/3/21.
+//  Copyright © 2017年 m.zintao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
